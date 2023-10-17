95e467329a5391fb05e38c660a993df465eb278d
paul-charp <119979412+paul-charp@users.noreply.github.com>
2023-10-17 22:19:02 +0200

    Logger bux fixes

--- ms_solaris_plugin/python3.9libs/msusd/Logger.py
+++ ms_solaris_plugin/python3.9libs/msusd/Logger.py
@@ -7,8 +7,8 @@ class Logger:
 
     source_name = "MSUSD"
     log_file = "$HOUDINI_TEMP_DIR/msusd.log"
-    default_console_verbosity = 3
-    console_verbosity_map = {
+    default_console_verbosity = 0
+    hou_severity_map = {
         0: hou.severityType.Message,
         1: hou.severityType.ImportantMessage,
         2: hou.severityType.Warning,
@@ -16,7 +16,7 @@ class Logger:
         4: hou.severityType.Fatal,
     }
 
-    def __init__(self, context, console_verbosity=hou.severityType.Message):
+    def __init__(self, context):
         if Logger.__loggers.get(context) != None:
             Logger.getLogger(context)
 
@@ -24,26 +24,18 @@ class Logger:
 
         self.context = context
 
-        console_verbosity_settings = hou.getenv("MSUSD_CONSOLE_VERBOSITY")
-        if (
-            console_verbosity_settings == None
-            and console_verbosity_settings not in console_verbosity_map.keys()
-        ):
-            console_verbosity_settings = Logger.default_console_verbosity
+        self.setConsoleVerbosity(4)
 
-        hou_console_verbosity_settings = console_verbosity_map[
-            console_verbosity_settings
-        ]
-        self.console_verbosity = min(hou_console_verbosity_settings, console_verbosity)
+        # if Logger.source_name not in hou.logging.sources():
+        hou.logging.createSource(Logger.source_name)
 
-        if Logger.source_name not in hou.logging.sources():
-            hou.logging.createSource(Logger.source_name)
+        # if hou.getenv("MSUSD_LOG_FILE") == 1:
 
-            if hou.getenv("MSUSD_LOG_FILE"):
-                Logger._initFileSink()
+        self.message(f"Logger for {self.context} initialized")
+        Logger._initFileSink()
 
     @staticmethod
-    def getLogger(context):
+    def getLogger(context) -> "Logger":
         if Logger.__loggers.get(context) == None:
             Logger(context)
         return Logger.__loggers.get(context)
@@ -51,12 +43,12 @@ class Logger:
     def log(self, message, severity=None):
         log_entry = hou.logging.LogEntry(
             message=message,
-            source_name=Logger.source_name,
+            source=Logger.source_name,
             source_context=self.context,
             severity=severity,
         )
 
-        hou.logging.log(log_entry)
+        hou.logging.log(log_entry, source_name=Logger.source_name)
 
         if severity != None:
             if severity < self.console_verbosity:
@@ -65,27 +57,25 @@ class Logger:
         return log_entry
 
     def fatal(self, message):
-        return self.log(self, message, severity=hou.severityType.Fatal)
+        return self.log(message, severity=hou.severityType.Fatal)
 
     def error(self, message):
-        return self.log(self, message, severity=hou.severityType.Error)
+        return self.log(message, severity=hou.severityType.Error)
 
     def warning(self, message):
-        return self.log(self, message, severity=hou.severityType.Warning)
+        return self.log(message, severity=hou.severityType.Warning)
 
     def importantMessage(self, message):
-        return self.log(self, message, severity=hou.severityType.ImportantMessage)
+        return self.log(message, severity=hou.severityType.ImportantMessage)
 
     def message(self, message):
-        return self.log(self, message, severity=hou.severityType.Message)
+        return self.log(message, severity=hou.severityType.Message)
 
     @staticmethod
     def _initFileSink():
         log_file = hou.text.expandString(Logger.log_file)
-
         file_sink = hou.logging.FileSink(log_file)
         file_sink.connect(Logger.source_name)
-
         return file_sink
 
     @staticmethod
@@ -98,6 +88,29 @@ class Logger:
         dt_object = datetime.datetime.fromtimestamp(time)
         time = dt_object.strftime("%H:%M:%S.%f")[:-3]
 
-        severity = str(severity).split(".")[-1]
+        severity = str(severity).split(".")[-1].upper()
 
         print(time, severity, context, message)
+
+    @staticmethod
+    def _getDefaultConsoleVerbosity():
+        default_console_verbosity = hou.getenv("MSUSD_CONSOLE_VERBOSITY")
+        if (
+            default_console_verbosity == None
+            or default_console_verbosity not in Logger.hou_severity_map.keys()
+        ):
+            default_console_verbosity = Logger.default_console_verbosity
+
+        return default_console_verbosity
+
+    @staticmethod
+    def _getHouSeverity(key):
+        return Logger.hou_severity_map[key]
+
+    def setConsoleVerbosity(self, console_verbosity):
+        default_verbosity = Logger._getDefaultConsoleVerbosity()
+
+        console_verbosity = min(default_verbosity, console_verbosity)
+
+        hou_severity = Logger._getHouSeverity(console_verbosity)
+        self.console_verbosity = hou_severity
--- ms_solaris_plugin/python3.9libs/msusd/MsPlugin.py
+++ ms_solaris_plugin/python3.9libs/msusd/MsPlugin.py
@@ -2,9 +2,22 @@ from . import SocketListener
 from .UI.MainWindow import MSMainWindow
 from .Utils.jsondebug import tmp_json_write
 from . import SettingsManager
+from .Logger import Logger
+
+import hou
 
 
 def initializePlugin():
+    logger = Logger.getLogger("Debug")
+    logger.setConsoleVerbosity(0)
+
+    logger.fatal("Fatal")
+    logger.error("Error")
+    logger.warning("Warning")
+    logger.importantMessage("ImportantMessage")
+    logger.message("Message")
+    logger.log("Just a log")
+
     # Get SettingsManager
     settings = SettingsManager.getInstance()
     # Init MainWindow
