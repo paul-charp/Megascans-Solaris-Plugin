bcc89a601182b0db8f5dd340b37858df51e7a02e
Paul Charpentier <paul.charpentier.99@gmail.com>
2023-10-18 18:00:59 +0200

    Remove logger debug code

--- ms_solaris_plugin/python3.9libs/msusd/MsPlugin.py
+++ ms_solaris_plugin/python3.9libs/msusd/MsPlugin.py
@@ -6,18 +6,12 @@ from .UI.MainWindow import MSMainWindow
 
 
 def initializePlugin():
-    logger = Logger.getLogger("Debug")
-    logger.setConsoleVerbosity(4)
-
-    logger.fatal("Fatal")
-    logger.error("Error")
-    logger.warning("Warning")
-    logger.importantMessage("ImportantMessage")
-    logger.message("Message")
-    logger.log("Just a log")
+    # Get Logger
+    logger = Logger.getLogger("Plugin")
 
     # Get SettingsManager
     settings = SettingsManager.getInstance()
+
     # Init MainWindow
     mWindow = MSMainWindow.getInstance()
     mWindow.show()
