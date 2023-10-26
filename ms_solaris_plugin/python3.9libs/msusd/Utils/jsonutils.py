8e0aa023a8f34b76f0fef7a057a50d9a0005338e
Paul Charpentier <paul.charpentier.99@gmail.com>
2023-10-26 12:33:34 +0200

    Comment jsonutils

--- ms_solaris_plugin/python3.9libs/msusd/Utils/jsonutils.py
+++ ms_solaris_plugin/python3.9libs/msusd/Utils/jsonutils.py
@@ -2,7 +2,16 @@ import json
 from ..Logger import Logger
 
 
-def write_json(d: dict, filepath: str, indent=None, logger=None):
+def write_json(d: dict, filepath: str, indent: int = None, logger: Logger = None):
+    """
+    Writes a dictionary to a JSON file.
+
+    Args:
+        d: dict: Specify the dictionary that will be written to a file
+        filepath: str: Specify the filepath of the json file to be written
+        indent: int: Specify the indentation level of the json file (defaults to None)
+        logger: Logger: Log a message using the provided logger if not None (defaults to None)
+    """
     with open(filepath, "w+") as json_file:
         json.dump(d, json_file, indent=indent)
 
@@ -10,7 +19,17 @@ def write_json(d: dict, filepath: str, indent=None, logger=None):
         logger.message(f"Write {filepath}")
 
 
-def read_json(filepath: str, logger=None) -> dict:
+def read_json(filepath: str, logger: Logger = None) -> dict:
+    """
+    Reads a JSON file and returns the data as a dictionary.
+
+    Args:
+        filepath: str: Specify the filepath of the json file to be read
+        logger: Logger: Log a message using the provided logger if not None (defaults to None)
+
+    Returns:
+        A dictionary
+    """
     with open(filepath, "r") as json_data:
         d = json.load(json_data)
 
