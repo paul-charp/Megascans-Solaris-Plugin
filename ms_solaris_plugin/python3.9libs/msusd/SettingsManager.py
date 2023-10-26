26a6f29edfdb7fbdaa117dab8686ac7e04982af2
Paul Charpentier <paul.charpentier.99@gmail.com>
2023-10-26 12:08:24 +0200

    Documented SettingsManager

--- ms_solaris_plugin/python3.9libs/msusd/SettingsManager.py
+++ ms_solaris_plugin/python3.9libs/msusd/SettingsManager.py
@@ -1,11 +1,18 @@
 import os
 import hou
-import json
 from .Utils import dictutils, jsonutils
 from .Logger import Logger
 
 
 class SettingsManager:
+    """
+    Settings Manager class.
+        Use SettingsManager.getInstance() to get or create the settings manager.
+        The Settings Manager hold the settings loaded from the settings file.
+        It guarantees that all the keys of the default settings are present in the provided settings as well a the values types.
+        Default settings and settings file location are defined in this class.
+    """
+
     __instance = None
 
     defaultSettings = {
@@ -17,6 +24,15 @@ class SettingsManager:
     settingsFile = "$HOUDINI_USER_PREF_DIR/msusd_settings.json"
 
     def __init__(self):
+        """
+        Sets up the instance of SettingsManager, and loads settings from a file if it exists.
+            If no settings file exists, it creates one with default values.
+            if the settings file exists but its settings are not validated,
+                it loads the default settings without changing the settings file
+
+        Args:
+            self: Represent the instance of the class
+        """
         if SettingsManager.__instance != None:
             SettingsManager.getInstance()
 
@@ -46,12 +62,26 @@ class SettingsManager:
 
     @staticmethod
     def getInstance():
+        """
+        Static method that returns the singleton instance of the SettingsManager class.
+            If no instance exists, it creates one and then returns it.
+
+        Returns:
+            An instance of the settingsmanager class
+        """
         if SettingsManager.__instance == None:
             SettingsManager()
         return SettingsManager.__instance
 
     @staticmethod
     def _getJsonSettings():
+        """
+        Private function that reads the settings file and returns it as a dictionary.
+            If the settings file does not exist, then None is returned.
+
+        Returns:
+            A dictionary
+        """
         settingsFile = hou.text.expandString(SettingsManager.settingsFile)
 
         if not os.path.isfile(settingsFile):
@@ -61,7 +91,20 @@ class SettingsManager:
         return json_data
 
     @staticmethod
-    def validateSettings(settings, defaultSettings=defaultSettings):
+    def validateSettings(
+        settings, defaultSettings=defaultSettings
+    ) -> tuple(bool, Exception):
+        """
+        Takes a settings dictionary and validates it against the defaultSettings.
+            Checks keys presence and value types.
+
+        Args:
+            settings: the settings to validate
+            defaultSettings: the settings to validated against
+
+        Returns:
+            A tuple of (bool, error)
+        """
         result, error = dictutils.validateDict(settings)
 
         if not result:
@@ -87,6 +130,14 @@ class SettingsManager:
         return (True, None)
 
     def putSettings(self, settings: dict):
+        """
+        Takes a dictionary of settings and merges it with the current settings.
+            The new values will overwrite any existing ones.
+
+        Args:
+            self: Represent the instance of the class
+            settings: dict: the settings to replace/merge
+        """
         result, error = dictutils.validateDict(settings)
 
         if not result:
@@ -96,9 +147,26 @@ class SettingsManager:
         self.__settings = merged
 
     def putSetting(self, key: str, value):
+        """
+        Set a value in the settings dictionary.
+            The key parameter is the name of the setting, and value is its new value.
+            If there are any sub-dictionaries in between, they will be created if necessary.
+
+        Args:
+            self: Represent the instance of the class
+            key: str: Set the key of the setting (navigate nested dicts with "." between keys)
+            value: Set the value of a key in the settings dictionary
+        """
         dictutils.setValue(self.__settings, key, value)
 
     def saveSettings(self):
+        """
+        Saves the settings to the json file.
+            The function takes no arguments and returns nothing.
+
+        Args:
+            self: Represent the instance of the class
+        """
         settingsFile = hou.text.expandString(SettingsManager.settingsFile)
 
         jsonutils.write_json(self.getSettings(), settingsFile, indent=4)
@@ -106,6 +174,17 @@ class SettingsManager:
         self.logger.message("Settings Saved")
 
     def getSettings(self, key=None):
+        """
+            If a key is provided, it will return the value of that key in the settings dictionary.
+                If not, it will return the complete settings dictionary.
+
+        Args:
+            self: Represent the instance of the class
+            key: Get a specific value from the settings dictionary
+
+        Returns:
+            The value of the key parameter or the settings dictionary
+        """
         if key != None:
             return dictutils.getValue(self.__settings, key)
         return self.__settings
